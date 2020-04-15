66ab2bd636f7f04dc0b59b21ff67926ed7b9938d
Sam Gwilliam <sambc49@gmail.com>
2020-04-15 15:24:38 +0100

    Adding first test - check if number is Fizz

---
+++ FizzBuzz/Services/FizzBuzzProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FizzBuzz.Services
+{
+    public class FizzBuzzProvider
+    {
+        public bool IsFizz(int number)
+        {
+            return number % 3 == 0;
+        }
+    }
+}
---
+++ UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,33 @@
+using FizzBuzz.Services;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        private int _maxCount = 100;
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void IsFizzShouldBeTrue()
+        {
+            //arrange
+            var service = BuildService();
+            bool expected = true;
+
+            //act
+            var result = service.IsFizz(3);
+
+            //assert
+            Assert.AreEqual(expected, result);
+        }
+
+        private FizzBuzzProvider BuildService()
+        {
+            return new FizzBuzzProvider();
+        }
+    }
+}
